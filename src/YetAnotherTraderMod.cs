a746b5f11af846c1174b380957c793332c1f3242
AMightyTank <chrisyoungplus@outlook.com>
2026-04-08 10:36:45 -0500

    Update YetAnotherTraderMod.cs

--- src/YetAnotherTraderMod.cs
+++ src/YetAnotherTraderMod.cs
@@ -1,3 +1,5 @@
+using CommonLibExtended.Services;
+using CommonLibExtended.Traders.Models;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Helpers;
@@ -8,8 +10,8 @@ using SPTarkov.Server.Core.Routers;
 using SPTarkov.Server.Core.Servers;
 using SPTarkov.Server.Core.Utils;
 using System.Reflection;
-using Range = SemanticVersioning.Range;
 using Path = System.IO.Path;
+using Range = SemanticVersioning.Range;
 
 namespace YetAnotherTraderMod.src;
 
@@ -31,52 +33,37 @@ public record ModMetadata : AbstractModMetadata
     public override string License { get; init; } = "MIT";
 }
 
-[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 4)]
 public sealed class YetAnotherTraderModMod(
     ModHelper modHelper,
     ImageRouter imageRouter,
     ConfigServer configServer,
-    TimeUtil timeUtil,
-    TraderHelper traderHelper) : IOnLoad
+    CLETraderBootstrap traderBootstrap)
+    : IOnLoad
 {
     private readonly TraderConfig _traderConfig = configServer.GetConfig<TraderConfig>();
     private readonly RagfairConfig _ragfairConfig = configServer.GetConfig<RagfairConfig>();
 
     public Task OnLoad()
     {
-        // A path to the mods files we use below
-        var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        var assembly = Assembly.GetExecutingAssembly();
+        var modRoot = modHelper.GetAbsolutePathToModFolder(assembly);
 
-        // A relative path to the trader icon to show
-        var traderImagePath = Path.Combine(pathToMod, "db/trader.png");
+        traderBootstrap.LoadTrader(
+            assembly,
+            Path.Join("db", "base.json"),
+            Path.Join("db", "assort.json"),
+            Path.Join("config", "settings.json"),
+            _traderConfig,
+            _ragfairConfig,
+            path => modHelper.GetJsonDataFromFile<TraderBase>(modRoot, Path.GetRelativePath(modRoot, path)),
+            path => modHelper.GetJsonDataFromFile<TraderAssort>(modRoot, Path.GetRelativePath(modRoot, path)),
+            path => modHelper.GetJsonDataFromFile<CustomTraderSettings>(modRoot, Path.GetRelativePath(modRoot, path)),
+            firstName: "Priscilu",
+            description: "",
+            imageRouter: imageRouter,
+            traderImageRelativePath: Path.Join("db", "trader.png"));
 
-        // The base json containing trader settings we will add to the server
-        var traderBase = modHelper.GetJsonDataFromFile<TraderBase>(pathToMod, "db/base.json");
-
-        // Create a helper class and use it to register our traders image/icon + set its stock refresh time
-        imageRouter.AddRoute(traderBase.Avatar.Replace(".png", ""), traderImagePath);
-        traderHelper.SetTraderUpdateTime(_traderConfig, traderBase, timeUtil.GetHoursAsSeconds(1), timeUtil.GetHoursAsSeconds(2));
-
-        // Add our trader to the config file, this lets it be seen by the flea market
-        _ragfairConfig.Traders.TryAdd(traderBase.Id, true);
-
-        // Add our trader (with no items yet) to the server database
-        // An 'assort' is the term used to describe the offers a trader sells, it has 3 parts to an assort
-        // 1: The item
-        // 2: The barter scheme, cost of the item (money or barter)
-        // 3: The Loyalty level, what rep level is required to buy the item from trader
-        traderHelper.AddTraderWithEmptyAssortToDb(traderBase);
-
-        // Add localisation text for our trader to the database so it shows to people playing in different languages
-        traderHelper.AddTraderToLocales(traderBase, "Tony", "DevGru SOCOM JSOC Armory");
-
-        // Get the assort data from JSON
-        var assort = modHelper.GetJsonDataFromFile<TraderAssort>(pathToMod, "db/assort.json");
-
-        // Save the data we loaded above into the trader we've made
-        traderHelper.OverwriteTraderAssort(traderBase.Id, assort);
-
-        // Send back a success to the server to say our trader is good to go
         return Task.CompletedTask;
     }
 }
